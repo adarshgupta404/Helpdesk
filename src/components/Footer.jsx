4809ac4501f0b29adb3e5009b1e2a0fc7eec1e1d
adarshgupta404 <gadarsh747@gmail.com>
2023-12-22 18:18:09 +0530

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#55D6C2] relative h-12 md:w-[calc(100vw-160px)] flex justify-center items-center font-semibold'>Footer</div>
+    <div className='bg-[#55D6C2] sticky bottom-0 h-12 md:w-[calc(100vw-160px)] flex justify-center items-center font-semibold'>Footer</div>
   )
 }
 
